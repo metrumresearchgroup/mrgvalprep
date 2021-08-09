3e8f376a26388f78bcda7250f7e6b26765c7130a
seth127 <seth127@gmail.com>
2021-08-09 12:39:16 -0400

    test: adding test for parse_github_issues()

--- tests/testthat/test-parse-github-issues.R
+++ tests/testthat/test-parse-github-issues.R
@@ -1,9 +1,35 @@
 test_that("get_issues() pulls from Github", {
   release_issues <- get_issues(org = ORG, repo = REPO, mile = MILESTONE, domain = DOMAIN)
+  expect_equal(nrow(spec), STORIES_DF_ROWS)
+
   # check a few things that likely won't change if we update some stories
-  expect_true(nrow(release_issues) > 5)
   expect_true(all(stringr::str_detect(release_issues$body, stringr::regex("Summary.+Tests", dotall = TRUE))))
   expect_true(all(stringr::str_detect(release_issues$milestone, MILESTONE)))
   expect_true(all(stringr::str_detect(release_issues$resource_path, REPO)))
+
+})
+
+test_that("parse_github_issues() pulls from Github", {
+  spec <- parse_github_issues(org = ORG, repo = REPO, mile = MILESTONE, domain = DOMAIN)
+  expect_equal(nrow(spec), STORIES_DF_ROWS)
+  expect_equal(ncol(spec), STORIES_DF_COLS)
+
+  # check that all cols except TestIds are full of text
+  purrr::walk(names(spec)[1:ncol(spec)-1], function(.n) {
+    expect_true(inherits(spec[[.n]], "character"))
+    expect_true(all(purrr::map_lgl(spec[[.n]], ~!is.null(.x))))
+    expect_true(all(purrr::map_lgl(spec[[.n]], ~nchar(.x) > 1)))
+  })
+
+  # check that TestIds are all character vectors except the last (empty) one
+  expect_true(inherits(spec[["TestIds"]], "list"))
+  expect_equal(
+    purrr::map_lgl(spec[["TestIds"]], ~!is.null(.x)),
+    c(TRUE,  TRUE,  TRUE,  TRUE,  TRUE, FALSE)
+  )
+  expect_equal(
+    purrr::map_lgl(spec[["TestIds"]], ~length(.x) > 1),
+    c(TRUE,  TRUE,  TRUE,  TRUE,  TRUE, FALSE)
+  )
 
 })
