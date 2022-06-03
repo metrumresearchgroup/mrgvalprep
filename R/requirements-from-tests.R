41415bb14f7fa58450982816d3b7b56257ff15c2
seth127 <seth127@gmail.com>
2022-06-03 15:21:06 -0400

    feat: adding requirements from tests functions, with docs and tests.

---
+++ R/requirements-from-tests.R
@@ -0,0 +1,94 @@
+################################################################
+# Helper functions for incorporating requirements
+# into a repo that previously only tracked stories and tests
+################################################################
+
+#' @name requirements_from_tests
+#' @title Requirements from tests
+#'
+#' @description If you were previously linking stories directly to tests, and
+#'   you would like to begin using requirements, use [req_df_from_tests()] and
+#'   [req_df_to_yaml()] to create a requirements YAML file that parses
+#'   requirement descriptions from test names. Then use
+#'   [stories_replace_tests_with_reqs()] to edit your stories YAML to reference
+#'   the newly created requirements.
+#'
+#' @details Note that these functions are designed to help you convert a repo
+#' that previously did _not_ use requirements into one that does. They are not
+#' meant for continued use after that. Going forward, requirements should be
+#' specified in the requirements YAML file, instead of being pull through from
+#' test names
+#'
+#' @examples
+#' \dontrun{
+#'
+#' # run your test suite to get a tibble of test names and IDs
+#' test_res <- mrgvalprep::parse_testthat_list_reporter(
+#'   devtools::test(Reporter = testthat::ListReporter)
+#' )
+#'
+#' # create requirements tibble and write to new YAML
+#' req_df <- req_df_from_tests(test_res)
+#' req_df_to_yaml(req_df, "inst/validation/requirements.yaml")
+#'
+#' # edit existing stories YAML
+#' stories_replace_tests_with_reqs("inst/validation/stories.yaml", req_df)
+#'
+#'}
+NULL
+
+#' @describeIn requirements_from_tests Create new requirements tibble from test
+#'   results tibble.
+#' @export
+req_df_from_tests <- function(test_df) {
+  test_df %>%
+    mutate(
+      req_id = .data$TestId %>%
+        stringr::str_replace("[A-Za-z]+\\-", "") %>%
+        stringr::str_replace("([0-9]+)$", "R\\1"),
+      req_desc = .data$TestName,
+      test_ids = .data$TestId
+    ) %>%
+    select(req_id, req_desc, test_ids)
+}
+
+#' @describeIn requirements_from_tests Write requirements YAML file from
+#'   requirements tibble.
+#' @export
+req_df_to_yaml <- function(req_df, out_file) {
+  req_df %>%
+    purrr::pmap(function(...) {
+      .row <- list(...)
+      list(
+        description = .row$req_desc,
+        tests = as.list(.row$test_ids)
+      )
+    }) %>%
+    rlang::set_names(req_df$req_id) %>%
+    yaml::write_yaml(out_file)
+  message(paste("Requirements YAML written to", out_file))
+}
+
+#' @describeIn requirements_from_tests Modify existing stories YAML to reference
+#'   new requirements instead of tests.
+#' @export
+stories_replace_tests_with_reqs <- function(stories_file, req_spec) {
+  stories_str <- readr::read_lines(stories_file)
+
+  stories_str <- stringr::str_replace_all(
+    stories_str,
+    "tests:",
+    "requirements:"
+  )
+
+  for (i in 1:nrow(req_spec)) {
+    stories_str <- stringr::str_replace_all(
+      stories_str,
+      req_spec$test_ids[i],
+      req_spec$req_id[i]
+    )
+  }
+
+  readr::write_lines(stories_str, stories_file)
+  message(paste("Edited Stories YAML file:", stories_file))
+}
