22e560d51a347024aa78b9962c5718d97bef4ed8
barrettk <barrettk@metrumrg.com>
2023-05-02 13:31:41 -0400

    patch: allow for multiple testing directories  - export get_testing_dir

--- R/get-tests.R
+++ R/get-tests.R
@@ -1,9 +1,9 @@
 
 #' Get tests/testthat directory from package directory
 #'
-#' @inheritParams get_tests
+#' @param pkg_dir a file path pointing to an unpacked/untarred package directory
 #'
-#' @keywords internal
+#' @export
 get_testing_dir <- function(pkg_dir){
   checkmate::assert_directory_exists(pkg_dir)
 
@@ -24,45 +24,68 @@ get_testing_dir <- function(pkg_dir){
 
 #' Tabulate all tests in a package directory
 #'
-#' @param pkg_dir a file path pointing to an unpacked/untarred package directory
+#' @inheritParams get_testing_dir
+#' @param test_dirs one or more directories containing test scripts
+#'
+#' @details
+#' Note that `get_testing_dir` will only return the directory `tests/testthat` (if it exists).
+#' You must specify other directories manually if you have multiple.
+#'
+#' ```
+#' # Example
+#' test_dirs = c(
+#'   get_testing_dir(pkg_dir),
+#'   'inst/other-tests'
+#' )
+#' ```
 #'
 #' @returns a dataframe containing all test names and the test file they are associated with
 #'
 #' @export
-get_tests <- function(pkg_dir){
-  test_dir <- get_testing_dir(pkg_dir)
-  test_files <- fs::dir_ls(test_dir)
-  test_scripts <- test_files[grep("\\.R$", test_files, ignore.case = TRUE)]
+get_tests <- function(
+    pkg_dir,
+    test_dirs = get_testing_dir(pkg_dir)
+){
 
-  tests_names_lst <- purrr::map(test_scripts, ~{
-    test_script <- readLines(.x)
+  test_names_df <- purrr::map_dfr(test_dirs, function(test_dir_x){
+    test_files <- fs::dir_ls(test_dir_x)
+    test_scripts <- test_files[grep("\\.R$", test_files, ignore.case = TRUE)]
 
-    # Find test_that calls and extract the names of the tests
-    test_that_calls <- grep('(?:\\s|^)test_that\\("([^"]+)"', test_script)
-    test_that_names <- test_script[test_that_calls]
+    tests_names_lst <- purrr::map(test_scripts, ~{
+      test_script <- readLines(.x)
 
-    # Find it calls and extract the names of the tests
-    it_calls <- grep('(?:\\s|^)it\\("([^"]+)"', test_script)
-    it_names <- test_script[it_calls]
+      # Find test_that calls and extract the names of the tests
+      test_that_calls <- grep('(?:\\s|^)test_that\\("([^"]+)"', test_script)
+      test_that_names <- test_script[test_that_calls]
 
-    # Combine the names of tests and count the number of tests in each file
-    n_tests <- length(c(test_that_names[test_that_calls], it_names[it_calls]))
-    test_names <- c(test_that_names, it_names)
+      # Find it calls and extract the names of the tests
+      it_calls <- grep('(?:\\s|^)it\\("([^"]+)"', test_script)
+      it_names <- test_script[it_calls]
 
-    # Remove `test_that` and `it` calls and code
-    stringr::str_extract_all(test_names, "\"([^\"]*)\"|'([^']*)'") %>%
-      gsub("[\"',/{]", "", .)
-  }) %>% stats::setNames(basename(test_scripts))
+      # Combine the names of tests and count the number of tests in each file
+      n_tests <- length(c(test_that_names[test_that_calls], it_names[it_calls]))
+      test_names <- c(test_that_names, it_names)
 
-  # Remove empty scripts (setup/non-test scripts)
-  tests_names_lst <- Filter(Negate(rlang::is_empty), tests_names_lst)
+      # Remove `test_that` and `it` calls and code
+      stringr::str_extract_all(test_names, "\"([^\"]*)\"|'([^']*)'") %>%
+        gsub("[\"',/{]", "", .)
+    }) %>% stats::setNames(basename(test_scripts))
 
-  # Convert to dataframe
-  test_names_tbl <- purrr::map2_df(tests_names_lst, names(tests_names_lst),
-                                   ~ tibble(result_file = .y, TestName = .x))
+    # Remove empty scripts (setup/non-test scripts)
+    tests_names_lst <- Filter(Negate(rlang::is_empty), tests_names_lst)
 
-  # Make sure no elements were lost (this can be part of a test)
-  checkmate::assert_true(all(unname(unlist(tests_names_lst)) %in% test_names_tbl$TestName))
+    # Convert to dataframe
+    test_names_tbl <- purrr::map2_df(tests_names_lst, names(tests_names_lst),
+                                     ~ tibble(result_file = .y, TestName = .x)) %>%
+      dplyr::mutate(test_dir = fs::path_rel(test_dir_x, start = pkg_dir))
 
-  return(test_names_tbl)
+    # Make sure no elements were lost (this can be part of a test)
+    checkmate::assert_true(all(unname(unlist(tests_names_lst)) %in% test_names_tbl$TestName))
+
+    test_names_tbl
+  })
+
+  test_names_df
+
+  return(test_names_df)
 }
